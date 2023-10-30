cc94bfd724a5669bd3c7902d1f432ef5f27421d3
MeotJH <kimjh0417@gsitm.com>
2023-10-30 17:52:12 +0900

    [기능] chart.js라이브러리 선2개 예제

--- src/main/java/com/server/quant_bot/quant/trend_following/controller/TrendFollowController.java
+++ src/main/java/com/server/quant_bot/quant/trend_following/controller/TrendFollowController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/trend-follow")
--- src/main/java/com/server/quant_bot/view/ViewController.java
+++ src/main/java/com/server/quant_bot/view/ViewController.java
@@ -1,10 +1,8 @@
 package com.server.quant_bot.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller

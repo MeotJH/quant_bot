6e83004ba2ad7dadcfb5244cc2c95913ea3ed58a
meot <mallangyi@naver.com>
2023-11-02 21:21:17 +0900

    [리팩토링]한국경제 증권 크롤링 Url

--- src/main/java/com/server/quant_bot/crawling/service/NewsCrawling.java
+++ src/main/java/com/server/quant_bot/crawling/service/NewsCrawling.java
@@ -1,11 +1,10 @@
 package com.server.quant_bot.crawling.service;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -13,18 +12,20 @@ import java.util.*;
 @Service
 public class NewsCrawling implements Crawling{
 
+    @Value("${finance.news.url}")
+    private String FINANCE_NEWS_URL;
+
     List<Map> news = new ArrayList<>();
 
+
+
     public List<Map> get(){
-        // 크롤링 할 사이트 주소.
-        String url = "https://www.hankyung.com/finance";
 
         Document doc = null;
-
         try {
-            doc = Jsoup.connect(url).get();
+            doc = Jsoup.connect(FINANCE_NEWS_URL).get();
         } catch (Exception e) {
-            System.out.println(e);
+            throw new RuntimeException("크롤링 에서 에러가 발생했다!");
         }
         //재귀함수로 리팩
         Elements elements = doc.select("ul.news-list");

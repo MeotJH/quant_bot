1e5caa0f2ed5c34cbc4a4036f5ab9d65352b2955
김진한 <kimjh0417@gsitm.com>
2024-02-21 17:28:17 +0900

    [기능] 컨트롤러 변수들 로그찍기 기능

---
+++ src/main/java/com/server/quant_bot/comm/aop/Controllerlogger.java
@@ -0,0 +1,33 @@
+package com.server.quant_bot.comm.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class Controllerlogger {
+
+    //controller 패키지에 있는 모든 메소드들
+    @Pointcut("execution(* com.server.quant_bot..controller..*(..))")
+    private void cut(){}
+
+    @Around("cut()")
+    public Object controllerArgsLogger(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("In Controller");
+        Signature signature = joinPoint.getSignature();
+
+        log.info(signature.getDeclaringTypeName());
+        Object[] args = joinPoint.getArgs();
+
+        for (Object each: args){
+            log.info(each.toString());
+        }
+        return joinPoint.proceed();
+    }
+}
